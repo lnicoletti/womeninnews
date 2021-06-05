8abd98dfa0e812ba538b0f8276a0719d85b6b918
lnicoletti <leo.nicoletti@hotmail.fr>
2021-06-05 05:09:35 +0200

    added ruler and events to timeline (still needs work)

--- visualization/temp_words_concept2.js
+++ visualization/temp_words_concept2.js
@@ -1,6 +1,6 @@
 // dimensions
-margin = ({top: 100, bottom: 20, left: 40, right: 40})
-visWidth = 900 - margin.left - margin.right
+margin = ({top: 400, bottom: 20, left: 40, right: 40})
+visWidth = 1200 - margin.left - margin.right
 visHeight = 10000 - margin.top - margin.bottom
 // structure of plots
 cols = 1
@@ -14,16 +14,110 @@ console.log(grid)
 row = d3.scaleBand()
     .domain(d3.range(rows))
     .range([0, visHeight])
-    .paddingInner(-1.2)
+    .paddingInner(-1.5)
 
 col = d3.scaleBand()
     .domain(d3.range(cols))
     .range([0, visWidth])
     .paddingInner(0.2) 
 
+// world events data
+numberOfCategories = 5
+categories = ["0", "1", "2", "3", "4"]
+dateRange = [new Date(2010, 0).getTime(), new Date(2019, 0).getTime()];
+
+eventsWorld = [
+       {uid: 1, 
+        name: "Horrifying gang rape and murder in New Delhi, India", 
+        category: 4, 
+        date: new Date(2012, 1, 0)},
+       {uid: 1, 
+        name: "Caitlyn Jenner comes out in an interview", 
+        category: 4, 
+        date: new Date(2015, 1, 0)},
+       {uid: 1, 
+        name: "#EndRapeCulture in South Africa", 
+        category: 4, 
+        date: new Date(2016, 1, 0)},
+       {uid: 2, 
+        name: "Trump: “Grab her by the pussy”", 
+        category: 4, 
+        date: new Date(2016, 1, 0)},
+       {uid: 1, 
+        name: "#MeToo in U.S.A.", 
+        category: 4, 
+        date: new Date(2017, 1, 0)},
+       {uid: 2, 
+        name: "Harvey Weinstein Trial", 
+        category: 4, 
+        date: new Date(2017, 1, 0)},
+       {uid: 3, 
+        name: "Release of Wonder Woman", 
+        category: 4, 
+        date: new Date(2017, 1, 0)},
+       {uid: 4, 
+        name: "#MeToo in India", 
+        category: 4, 
+        date: new Date(2017, 1, 0)},
+       {uid: 1, 
+        name: "Meghan Markle’s wedding to Prince Harry", 
+        category: 4, 
+        date: new Date(2018, 1, 0)},
+       {uid: 2, 
+        name: "Priyanka Chopra marries Nick Jonas", 
+        category: 4, 
+        date: new Date(2018, 1, 0)},
+       {uid: 3, 
+        name: "People v. Turner", 
+        category: 4, 
+        date: new Date(2018, 1, 0)},
+       {uid: 1, 
+        name: "Greta Thunberg: Climate Action Summit and sail to NYC", 
+        category: 4, 
+        date: new Date(2019, 1, 0)},
+       {uid: 1, 
+        name: "#SayHerName: Murder of Breonna Taylor", 
+        category: 4, 
+        date: new Date(2020, 1, 0)},
+       {uid: 2, 
+        name: "Meghan Markle: scandal with royal family", 
+        category: 4, 
+        date: new Date(2020, 1, 0)},
+       {uid: 3, 
+        name: "Kamala Harris: first vice president Black/Indian woman", 
+        category: 4, 
+        date: new Date(2020, 1, 0)},
+       {uid: 1, 
+        name: "International Women’s Cricket T20", 
+        category: 4, 
+        date: new Date(2021, 1, 0)},
+       {uid: 2, 
+        name: "Makiyah Bryant murder", 
+        category: 4, 
+        date: new Date(2021, 1, 0)}]
+
+events = eventsWorld.map(d=>{
+  return {
+    uid: d.uid,
+    name: d.name, 
+    category: Math.floor(Math.random() * numberOfCategories),
+    position: Math.round(Math.random()) === 1,
+    date: d.date
+  }
+})
+
+groupedEvents = d3.group(
+    events.filter(e => categories.includes(`${e.category}`)), 
+    d => new Date(d.date.getFullYear(), d.date.getMonth()).toDateString(),
+    d => d.position)
+
+
+
+console.log(events)
+
 // load and render data
 // d3.csv("../hosted_data/country_time_freq_rapi.csv", d3.autoType).then(function(dataset) {
-    d3.csv("../data/processed/country_time_freqrank_rapi.csv", d3.autoType).then(function(dataset) {
+d3.csv("../data/processed/country_time_freqrank_rapi.csv", d3.autoType).then(function(dataset) {
 
     filter_years = [2009, 2020]
     country = "USA"
@@ -50,6 +144,7 @@ col = d3.scaleBand()
 })
 
 // tooltip functions
+//// area charts hover
 function showTooltip(event, d) {
     
     d3.selectAll(".wordArea").attr("opacity", 0.25)
@@ -93,6 +188,86 @@ function hideTooltip(event, d) {
     d3.selectAll(".wordText").attr("opacity", 1)
     d3.selectAll(".incText").remove()
 }
+
+// timeline ruler
+const tooltip = d3
+                .select("body")
+                .append("div")
+                .attr("class", "tooltip")
+                // .style("font-size", "10pt")
+                // .style("font-family", "Lato")
+                .style("position", "absolute")
+                .style("text-align", "left")
+                // .style("width", "auto")
+                // .style("height", "auto")
+                .style("padding", "5px")
+                .style("background", "none")
+                .attr("border", "grey")
+                .style("border-width", 1)
+                .style("pointer-events", "none")
+                .style("opacity", 0)
+
+function timeRuler(event, d, g) {
+    
+    const ruler = g.append("g")
+                    .append("line")
+                    .attr("class", "timeRuler")
+                    .attr('transform', `translate(${col(0)}, -${margin.top/3})`)
+                    .attr("x1", x(d.date))
+                    .attr("x2", x(d.date))
+                    .attr("y1", 0)
+                    .attr("y2", visHeight)
+                    .attr("stroke", "grey")
+                    // .attr("fill", "grey")
+                    .attr("stroke-width", 20)
+                    .attr("opacity", 0.2)
+
+    // rect dimensions
+    const boxWidth = 200
+    const boxHeight = 100
+    
+    tooltip
+        .transition()
+        .duration(0)
+        .style("opacity", 1)
+        .style("width", boxWidth)
+        .style("height", boxHeight)
+        
+    tooltip
+        // .html(`<b>${d.title}</b><br>
+        // <b>${format(+d[metric_t])+"</b> "+legend_label_t.toLowerCase()}<br>
+        // <b>${format(+d[metric_p])+"</b> "+legend_label_p.toLowerCase()}`)
+        .html(`<b>${d.date}</b><br>
+        <b>${(d.name)}`)
+        .style("left", event.pageX + 10+ "px")
+        .style("top", event.pageY - 28 + "px")
+        .style("font-family", "sans-serif")
+        .attr("font-size", "10px")
+        .style("color", "grey");
+
+    // const textBox = g.append("g")
+    //                   .append("rect")
+    //                   .attr('transform', `translate(${col(0)}, -${d.uid * 12 + margin.top/1.5})`)
+    //                   .attr("x", x(d.date)-boxWidth/2)
+    //                   .attr("y", 0)
+    //                   .attr("width", boxWidth)
+    //                   .attr("height", boxHeight)
+    //                 //   .attr("fill", "grey")
+    //                 //   .attr("fill", "none")
+    //                   .attr("stroke", "grey")
+    //                   .attr("opacity", 0.5)
+    //                 //   .attr("class", "timeRuler")
+
+    // const text = g.append("g")
+    //                     .append("text")
+    //                     .text(d.name)
+    //                     .attr("x", x(d.date)-boxWidth/2)
+    //                     .attr("y", 0)
+    //                     // .attr("font-family", "Georgia")
+    //                     .attr("font-size", "12px")
+    //                     .style("color", "white")
+}
+
 
 // small multiples chart function
 function renderChart(dataset, filter, country, variable) {
@@ -253,10 +428,17 @@ function renderChart(dataset, filter, country, variable) {
             .tickSizeOuter(20)
             .tickSizeInner(7)
             .tickPadding(20)
-            .tickFormat((d, i) => i == 0 || i == 9 ? d3.timeFormat('%Y')(new Date(d)): "");//.tickFormat(d3.format(".0s"))
+            .tickFormat((d, i) => i == 0 || i == 9 ? d3.timeFormat('%Y')(new Date(d)):
+                                //   i == 5 ? "Caitlyn Jenner comes out":
+                                //   i == 6 ? "Trump: 'Grab her by the pussy'":
+                                //   i == 7 ? "#MeToo":
+                                //   i == 8 ? "People v. Turner":
+                                //   i == 9 ? "Greta":
+                                //   i == 9 ? "#SayHerName":                                  
+                                  "");//.tickFormat(d3.format(".0s"))
 
     g.append("g")
-            .attr('transform', `translate(${col(0)}, -50)`)
+            .attr('transform', `translate(${col(0)}, -${margin.top/3})`)
             // .attr('transform', `translate(${margin.left}, ${margin.top})`)
             .call(xaxis).attr("class", "SMaxis")
             .call(g=>g.selectAll(".tick")
@@ -268,6 +450,33 @@ function renderChart(dataset, filter, country, variable) {
             .attr("font-size", 12))
             .call(g => g.select('.domain').attr("color", "grey").attr('stroke-width', 2))
             // .call(g => g.select('.domain').remove())
+
+    // circles for timeline
+    const circleEvents = 
+            g.append("g")
+                    .attr('transform', `translate(${col(0)}, -${margin.top/3})`)
+                    .selectAll("circle")
+                    .data(events)
+
+    const circles = circleEvents
+                    .join("circle")
+                    .attr('transform', (d, i) => `translate(0, ${d.uid * 12 * (d.position ? 1 : -1 )})`)
+                    .attr("cx", d=>x(d.date))
+                    .attr("cy", 0)
+                    .attr("fill", "red")
+                    .attr("r", 6)
+                    .attr("opacity", "0.5")
+                    .on("mouseover", (event, d) => timeRuler(event, d, g))
+                    // .on("mousemove", (event, d) => timeRuler(event, d, g))
+                    .on("mouseleave", (event, d) => {
+                                    d3.selectAll(".timeRuler").remove()
+                                    // tooltip
+                                    //         .transition()
+                                    //         .duration(500)
+                                    //         .style("opacity", 0)
+                                        }
+                                            )
+
 
      // add the y axis for each cell
     cells.each(function(d) {
