04c210815c5917405455744389ace955ae831ef8
lnicoletti <leo.nicoletti@hotmail.fr>
2021-08-24 23:12:08 +0200

    style fixes

--- final_app/index.js
+++ final_app/index.js
@@ -201,7 +201,7 @@
             // INJECT CSS
             var css = document.createElement("style");
             //css.type = "text/css";
-            css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+            css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666}";
             document.body.appendChild(css);
         };
 
